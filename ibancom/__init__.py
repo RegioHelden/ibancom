cae27606cf98af3ed0432b7f1fb822a28a9b5150
Mounir Messelmeni <messelmeni.mounir@gmail.com>
2018-01-06 17:31:07 +0100

    Bump version: 0.5.0 → 0.5.1

--- ibancom/__init__.py
+++ ibancom/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """RegioHelden GmbH"""
 __email__ = 'mounir.messelmeni@regiohelden.de'
-__version__ = '0.5.0'
+__version__ = '0.5.1'
--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ test_requirements = [
 
 setup(
     name='ibancom',
-    version='0.5.0',
+    version='0.5.1',
     description="Python client for IBAN.com service",
     long_description=readme + '\n\n' + history,
     author="RegioHelden GmbH",
