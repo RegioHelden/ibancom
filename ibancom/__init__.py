055e6e5aa3b204bf830140d29e01a0776240bb71
Mounir Messelmeni <messelmeni.mounir@gmail.com>
2018-01-08 18:29:10 +0100

    Bump version: 0.5.5 → 0.5.6

--- ibancom/__init__.py
+++ ibancom/__init__.py
@@ -3,7 +3,7 @@ from .ibancom import IBAN, IBANClient, IBANException, IBANValidationException
 
 __author__ = """RegioHelden GmbH"""
 __email__ = 'mounir.messelmeni@regiohelden.de'
-__version__ = '0.5.5'
+__version__ = '0.5.6'
 
 
 __all__ = [
